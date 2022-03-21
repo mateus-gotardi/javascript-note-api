e1428839f631a29e17b0f2200d8dc7abcf05411d
Mateus <mateus.gotardi@gmail.com>
2022-03-21 19:10:33 -0300

    api completa

---
+++ config/database.js
@@ -0,0 +1,7 @@
+const mongoose=require('mongoose')
+mongoose.Promise=global.Promise
+mongoose.connect('mongodb://localhost/javascriptNote',{
+    useNewUrlParser: true,
+    useUnifiedTopology:true,
+}).then(()=>{console.log('connection succesful')})
+.catch((err)=>{ console.log (err)})
