acb38790be174f405c824c5c91111633d426d735
Mateus <mateus.gotardi@gmail.com>
2022-04-25 13:50:25 -0300

    01

--- config/database.js
+++ config/database.js
@@ -1,11 +1,11 @@
-const mongoose=require('mongoose')
-mongoose.Promise=global.Promise
+const mongoose=require('mongoose');
+mongoose.Promise=global.Promise;
 
 require('dotenv').config();
-const MONGO_URL = process.env.MONGO_URL
+const MONGO_URL = process.env.MONGO_URL;
 
 mongoose.connect(MONGO_URL,{
     useNewUrlParser: true,
     useUnifiedTopology:true,
 }).then(()=>{console.log('connection succesful')})
-.catch((err)=>{ console.log (err)})
+.catch((err)=>{ console.log (err)});
