68c6c31311cab9c522cbc2fb249e9a7431d79bdd
Mateus <mateus.gotardi@gmail.com>
2022-04-25 14:09:12 -0300

    03

--- config/database.js
+++ config/database.js
@@ -7,7 +7,6 @@ const MONGO_URL = process.env.MONGO_URL;
 mongoose.connect(MONGO_URL,{
     useNewUrlParser: true,
     useUnifiedTopology:true,
-    serverApi: ServerApiVersion.v1
 }).then(()=>{console.log('connection succesful')})
 .catch((err)=>{ 
     console.log (MONGO_URL)
